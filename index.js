8b4697d2e43f90c80645d9b2a29f4a4868c20ee3
Kartikeya Pedaprolu <kartikeyapedaprolu@gmail.com>
2026-06-27 12:16:02 +0530

    Remove API key logging

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const ai = require("./gemini");
 const cors = require("cors");
-console.log(process.env.GEMINI_API_KEY);
+
 
 const app = express();
 
